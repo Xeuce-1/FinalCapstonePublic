60a810bd8b9ab4d1be15e6295723abe02cefb244
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-01 16:36:42 -0500

    Default navigation bar added.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="capstone-app">
+
+    <v-app>
+      <v-container>
+        <v-app-bar :elevation="2">Fan-Band Connector</v-app-bar>
+      </v-container>
+    </v-app>
+
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
