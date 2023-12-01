1a833d2004fdd0efc38b25e29c7b2c4cdfe3c1b9
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-01 17:23:25 -0500

    Vuetify icons installed.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="capstone-app">
+  <v-app>
+    <v-container>
+      <v-app-bar :elevation="2">
+        <v-app-bar-nav-icon />Fan-Band Connector</v-app-bar>
+    </v-container>
 
-    <v-app>
-      <v-container>
-        <v-app-bar :elevation="2">Fan-Band Connector</v-app-bar>
-      </v-container>
-    </v-app>
-
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    <div id="capstone-app">
+      <!-- <div id="nav">
+        <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+        <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      </div> -->
+      <router-view />
     </div>
-    <router-view />
-  </div>
+
+  </v-app>
 </template>
+
+<style scoped>
+#capstone-app {
+  margin-top: 3rem;
+}
+</style>
