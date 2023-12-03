755385d9b47048cf57e806bba67764d50166cb03
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-03 15:29:55 -0500

    Added a default layout for Vuetify. Contains a default navigation bar that will be shared across all routes.

---
+++ vue/src/layouts/default/DefaultAppBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app-bar flat border>
+        <v-app-bar-title>
+            <v-icon icon="mdi-menu" @click.stop="drawer = !drawer" />
+            FAN-BAND CONNECTOR
+        </v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" border temporary>
+        <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            items: []
+        }
+    }
+}
+</script>
---
+++ vue/src/layouts/default/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-app>
+        <DefaultAppBar />
+        <DefaultView />
+    </v-app>
+</template>
+
+<script setup>
+import DefaultAppBar from './DefaultAppBar.vue'
+import DefaultView from './DefaultView.vue'
+</script>
