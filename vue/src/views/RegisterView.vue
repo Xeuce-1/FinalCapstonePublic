59839283288c4ccbf37d24343acf077418ed0695
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-04 16:18:12 -0500

    WIP: Registration page styling

--- vue/src/views/RegisterView.vue
+++ vue/src/views/RegisterView.vue
@@ -1,6 +1,40 @@
 <template>
+  <v-card class="d-flex justify-center align-center h-screen mt-n16">
+    <v-card elevation="12" max-width="600" rounded="lg" width="100%"
+      class="pa-4 text-center mx-auto align-center justify-center">
+      <!-- <div id="register" class="text-center"> -->
+      <v-alert v-if="registrationErrors" type="error" density=“compact” closable>{{ registrationErrorMsg }}</v-alert>
+
+      <v-form @submit.prevent="register()">
+        <h1>Create Account</h1>
+
+        <!-- USERNAME INPUT -->
+        <v-text-field label="Username" v-model="user.username" :rules="usernameRules" id="username"></v-text-field>
+
+        <!-- PASSWORD INPUT -->
+        <v-text-field label="Password" type="password" v-model="user.password" :rules="passwordRules"
+          id="password"></v-text-field>
+
+        <!-- CONFIRM PASSWORD INPUT -->
+        <v-text-field label="Confirm Password" type="password" v-model="user.confirmPassword" id="confirmPassword"
+          validate-on="blur" :rules="confirmPasswordRules"></v-text-field>
+
+        <!-- SUBMIT BUTTON -->
+        <div class="pa-3">
+          <v-btn block type="submit" color="#f26419"> Create Account </v-btn>
+        </div>
+        <div class="pa-3">
+          <v-btn block color="#00afb9" @click.stop="toLoginPage">Already have an account? Log in.</v-btn>
+        </div>
+
+        <!-- registration message -->
+      </v-form>
+      <!-- </div> -->
+    </v-card>
+  </v-card>
+  <!--
   <div id="register" class="text-center">
-    <form v-on:submit.prevent="register">
+    <v-form v-on:submit.prevent="register">
       <h1>Create Account</h1>
       <div role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
@@ -19,8 +53,9 @@
       </div>
       <button type="submit">Create Account</button>
       <p><router-link v-bind:to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
-    </form>
+    </v-form>
   </div>
+-->
 </template>
 
 <script>
@@ -37,13 +72,37 @@ export default {
       },
       registrationErrors: false,
       registrationErrorMsg: 'There were problems registering this user.',
+      usernameRules: [
+        value => {
+          if (value) return true
+
+          return 'You must enter a valid email address.'
+        }
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 8) || 'Password must be at least 8 characters',
+        v => (v && v.length <= 20) || 'Password must be less than 20 characters',
+        v => (v && /[A-Z]/.test(v)) || 'Password must contain at least 1 uppercase letter',
+        v => (v && /[a-z]/.test(v)) || 'Password must contain at least 1 lowercase letter',
+        v => (v && /[0-9]/.test(v)) || 'Password must contain at least 1 number',
+      ],
+      confirmPasswordRules: [
+        () => {
+          if (this.user.password === this.user.confirmPassword) return true
+          return 'Password & Confirm Password do not match.'
+        }
+      ]
     };
   },
   methods: {
+    toLoginPage() {
+      this.$router.push({ name: 'login' });
+    },
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = 'Registration failure: passwords do not match.';
       } else {
         authService
           .register(this.user)
@@ -76,6 +135,7 @@ export default {
 .form-input-group {
   margin-bottom: 1rem;
 }
+
 label {
   margin-right: 0.5rem;
 }
