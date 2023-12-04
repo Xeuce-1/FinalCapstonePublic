ae8f5add711bcb5d119e74fb2282aaf2a7d4b126
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-04 10:36:50 -0500

    Nav bar updated to include login, logout, and a holder icon. Icons linked to their respective function views.

--- vue/src/layouts/default/DefaultAppBar.vue
+++ vue/src/layouts/default/DefaultAppBar.vue
@@ -6,7 +6,10 @@
         </v-app-bar-title>
 
         <template v-slot:append>
-            <v-btn icon="mdi-account" @click.stop="toLoginPage"></v-btn>
+            <v-btn icon="mdi-unicorn-variant"></v-btn>
+            <v-btn icon="mdi-login" @click.stop="toLoginPage"></v-btn>
+            <v-btn icon="mdi-logout" @click.stop="toLogoutPage"></v-btn>
+
         </template>
     </v-app-bar>
 
@@ -23,7 +26,7 @@
                 <v-btn block prepend-icon="mdi-login" color="#f26419" @click.stop="toLoginPage">Login</v-btn>
             </div>
             <div class="pa-2">
-                <v-btn block prepend-icon="mdi-logout" color="#f26419">Logout</v-btn>
+                <v-btn block prepend-icon="mdi-logout" color="#f26419" @click.stop="toLogoutPage">Logout</v-btn>
             </div>
         </template>
     </v-navigation-drawer>
@@ -40,6 +43,9 @@ export default {
     methods: {
         toLoginPage() {
             this.$router.push({ name: 'login' });
+        },
+        toLogoutPage() {
+            this.$router.push({ name: 'logout' })
         }
     }
 }
