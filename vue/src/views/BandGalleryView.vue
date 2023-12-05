e22ec1d41d92e9bde99d4fe17d4957db96b30e22
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-05 16:23:14 -0500

    Band Gallery View updated to popup image on overlay on click.

--- vue/src/views/BandGalleryView.vue
+++ vue/src/views/BandGalleryView.vue
@@ -1,15 +1,13 @@
 <template>
     <div id="page-background" class="pa-10">
         <v-row>
-            <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="3">
-                <PolaroidComponent class="h-100 w-100 " />
+            <v-col v-for="image in galleryImages" :key="image.id" class="d-flex child-flex" cols="3">
+                <PolaroidComponent class="h-100 w-100" :galleryImages="image.coverimageurl" />
                 <template v-slot:placeholder>
                     <v-row class=" fill-height ma-0" align="center" justify="center">
                         <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
                     </v-row>
                 </template>
-
-
             </v-col>
         </v-row>
     </div>
@@ -17,10 +15,19 @@
 
 <script>
 import PolaroidComponent from '../components/PolaroidComponent.vue'
+import BandService from '../services/BandService';
 
 export default {
+    data() {
+        return {
+            galleryImages: []
+        }
+    },
     components: {
         PolaroidComponent
+    },
+    created() {
+        BandService.getRandomImages().then(response => { this.galleryImages = response.data })
     }
 }
 </script>
--- vue/src/views/LandingView.vue
+++ vue/src/views/LandingView.vue
@@ -39,7 +39,6 @@ export default {
             images: [],
         }
     },
-
     created() {
         if (this.$store.state.token !== "") {
             this.$router.push({ name: 'home' });
