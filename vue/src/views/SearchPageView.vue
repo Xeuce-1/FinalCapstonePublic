35e561c5273cc932ee4049bd38d3b5fe03757365
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-07 09:50:22 -0500

    Committing to move to new branch.

--- vue/src/views/SearchPageView.vue
+++ vue/src/views/SearchPageView.vue
@@ -1,14 +1,16 @@
 <template>
     <h1 v-if="!isLoaded">LOADING...</h1>
-    <v-sheet v-else class="d-flex justify-center align-center h-screen mt-n16">
-        <v-sheet border class="h-50 w-75">
-            <v-btn-toggle v-model="toggle" color="#0081a7" divided class="ma-5 w-100">
-                <v-btn border>Band</v-btn> <!-- toggle: 0 -->
-                <v-btn border>Genre</v-btn>
+
+    <v-sheet v-else class="d-flex justify-center align-center h-screen mt-n16 flex-column">
+        <h1>Band Search</h1>
+        <v-sheet border class="d-flex justify-center align-center flex-column w-50">
+            <v-btn-toggle v-model="toggle" color="#0081a7" divided class="ma-5 w-100 d-flex justify-center">
+                <v-btn border class="w-25">Band</v-btn>
+                <v-btn border class="w-25">Genre</v-btn>
             </v-btn-toggle>
-            <v-autocomplete :items="bandsList" class="ma-10" prepend-inner-icon="mdi-magnify">SEARCH
+            <v-autocomplete :items="bandsList" class="w-75" prepend-inner-icon="mdi-magnify">SEARCH
                 BAR</v-autocomplete>
-            <v-btn block class="ma-10">SEARCH</v-btn>
+            <v-btn class="ma-10 w-50">SEARCH</v-btn>
         </v-sheet>
 
     </v-sheet>
