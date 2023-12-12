a60a51b9117a74f3229c86f8c3f2e1328ac56f6c
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-12 15:59:25 -0500

    WIP: working commit

--- vue/src/views/InboxView.vue
+++ vue/src/views/InboxView.vue
@@ -1,10 +1,11 @@
 <template>
-    <v-container v-if="isLoading" class="d-flex justify-center align-center h-screen mt-n16">
+    <v-container v-if="isLoading" class="d-flex justify-center align-center h-screen mt-n16 pa-0 ma-0">
         <v-progress-circular color="primary" indeterminate :size="93" :width="12"></v-progress-circular>
     </v-container>
-    <v-container v-else fluid class="fill-height d-flex flex-row flex-nowrap pa-0">
-        <v-sheet class="ma-0 w-25 h-100 pa-0" color="secondary">
-            <v-virtual-scroll :items="notifications" min-height="100%" max-height="100" item-height="48">
+    <v-container v-else fluid full-height class="fill-height d-flex flex-row flex-nowrap pa-0 ma-0" color="transparent">
+
+        <v-sheet class="ma-0 w-25 h-100" color="secondary">
+            <v-virtual-scroll :items="notifications" min-height="100%" max-height="100">
                 <template v-slot:default="{ item }">
                     <v-list-item :title="messageForBandName(item.bandId) + ': ' + item.subject"
                         :subtitle="item.dateAndTime + ': ' + item.description" lines="three"
@@ -15,13 +16,25 @@
             </v-virtual-scroll>
         </v-sheet>
 
-        <v-sheet class="w-75 h-100 pa-10" color="#d1bce3">
-            <div v-show="!isMessageSelected"></div>
-            <div v-show="isMessageSelected">
-                <h2>{{ selectedMessage[0].dateAndTime }}</h2>
-                <h1>{{ selectedBand[0].bandName }}: {{ selectedMessage[0].subject }}</h1>
-                <p>{{ selectedMessage[0].description }}</p>
-            </div>
+        <v-sheet class="w-75 h-100" color="#d1bce3">
+
+            <v-sheet class="d-flex align-center justify-center space-between pa-9" height="5%" color="#f26419">
+                <h2 class="mr-5">Filter Notifications: </h2>
+                <v-btn-toggle border v-model="toggle" divided>
+                    <v-btn @click="sortMessagesDateDescending">Date Descending</v-btn>
+                    <v-btn @click="sortMessagesDateAscending">Date Ascending</v-btn>
+                    <v-btn>Bands (A-Z)</v-btn>
+                </v-btn-toggle>
+            </v-sheet>
+            <v-container class="pa-10">
+                <div v-show="!isMessageSelected"></div>
+                <div v-show="isMessageSelected">
+                    <h2>{{ selectedMessage[0].dateAndTime }}</h2>
+                    <h1>{{ selectedBand[0].bandName }}: {{ selectedMessage[0].subject }}</h1>
+                    <p>{{ selectedMessage[0].description }}</p>
+                </div>
+            </v-container>
+
         </v-sheet>
 
     </v-container>
@@ -39,7 +52,8 @@ export default {
             notifications: [],
             selectedMessage: [-1],
             selectedBand: [-1],
-            bands: []
+            bands: [],
+            toggle: 0
         }
     },
     created() {
@@ -64,12 +78,14 @@ export default {
             return name[0].bandName;
         }
 
+    },
+    sortMessagesDateAscending() {
+        this.notifications.sort((a, b) => a.dateAndTime.localeCompare(b.dateAndTime));
+    },
+    sortMessagesDateDescending() {
+        this.notifications.sort((a, b) => b.dateAndTime - a.dateAndTime);
     }
 }
 </script>
 
-<style scoped>
-.test {
-    height: 84%;
-}
-</style>
+<style scoped></style>
