6c3c02ce366415bf84c1d0c3dbb556ddd53ff2d4
Ryan Young <ryanjonathanyoung@gmail.com>
2023-12-13 12:54:28 -0500

    Sort Inbox Completed

--- vue/src/views/InboxView.vue
+++ vue/src/views/InboxView.vue
@@ -6,7 +6,8 @@
     <v-container v-else fluid full-height class="fill-height d-flex flex-row flex-nowrap pa-0 ma-0" color="transparent">
 
         <v-sheet class="ma-0 w-25 h-100" color="secondary">
-            <v-virtual-scroll :items="notifications" v-model="notifications" min-height="100%" max-height="100">
+            <v-virtual-scroll :items="!filterByBandsActive ? notifications : filteredNotifications" v-model="notifications"
+                min-height="100%" max-height="100">
                 <template v-slot:default="{ item }">
                     <v-list-item :title="messageForBandName(item.bandId) + ': ' + item.subject"
                         :subtitle="item.dateAndTime + ': ' + item.description" lines="three"
@@ -18,23 +19,34 @@
         </v-sheet>
 
         <v-sheet class="w-75 h-100" color="#d1bce3">
-            <v-sheet class="d-flex align-center justify-center space-between pa-9" height="5%" color="#f26419">
-                <h2 class="mr-5">Filter Notifications: </h2>
-                <v-btn-toggle border v-model="toggle" divided>
-                    <v-btn @click="sortMessagesDateDescending">Date Descending</v-btn>
-                    <v-btn @click="sortMessagesDateAscending">Date Ascending</v-btn>
-                    <v-btn @click="sortMessagesByBandName">Bands (A-Z)</v-btn>
-                </v-btn-toggle>
-                <v-select :items="usersBands.name"></v-select>
 
+            <v-sheet class="d-flex align-center justify-start space-between pa-9" height="10%" color="#f26419">
+                <h2 class="mr-5">Filter Notifications: </h2>
+                <v-btn-toggle border v-model="toggle" divided color="secondary">
+                    <!-- DESCENDING BUTTON-->
+                    <v-btn @click="sortMessagesDateDescending">Date Descending</v-btn>
+
+                    <!-- ASCENDING BUTTON -->
+                    <v-btn @click="sortMessagesDateAscending">Date Ascending</v-btn>
+
+                    <!-- BY BAND BUTTON -->
+                    <v-btn @click="artistFilter = true">By Band</v-btn>
+
+                </v-btn-toggle>
+                <v-select bg-color="white" hide-details @update:menu="sortMessagesBySelectedBand()" base-color="secondary"
+                    :items="usersBands" variant="solo-filled" v-model="bandFilterQuery"
+                    class="ml-5 text-md-center rounded-xl" hide-selected persistent-placeholder="Select A Band"
+                    label="Filter By Band" :disabled="artistFilter === false">Band
+                    Filter</v-select>
             </v-sheet>
+
             <v-container class="pa-10">
                 <div v-show="!isMessageSelected"></div>
                 <div v-show="isMessageSelected">
                     <h2>{{ selectedMessage[0].dateAndTime }}</h2>
                     <h1>{{ selectedBand[0].bandName }}: {{ selectedMessage[0].subject }}</h1>
                     <p>{{ selectedMessage[0].description }}</p>
-                    {{ usersBands }}
+
                 </div>
             </v-container>
         </v-sheet>
@@ -50,6 +62,9 @@ import FollowerService from '../services/FollowerService';
 export default {
     data() {
         return {
+            artistFilter: false,
+            filterByBandsActive: false,
+            filteredNotifications: [],
             isLoading: true,
             isMessageSelected: false,
             notifications: [],
@@ -57,6 +72,7 @@ export default {
             selectedBand: [-1],
             usersBands: [],
             searchBands: [],
+            bandFilterQuery: 'Fallout Boy',
             bands: [],
             toggle: 0
         }
@@ -70,12 +86,13 @@ export default {
             .then(response => {
                 this.bands = response.data;
                 this.isLoading = false;
+
             });
         FollowerService.getUsersFollowers(this.$store.state.user.id)
             .then(response => {
                 const data = response.data;
                 data.forEach(element => {
-                    this.usersBands.push(element);
+                    this.usersBands.push(element.bandName)
                 });
             });
     },
@@ -90,16 +107,20 @@ export default {
             return name[0].bandName;
         },
         sortMessagesDateAscending() {
+            this.artistFilter = false;
+            this.filterByBandsActive = false;
             this.notifications.sort((a, b) => a.dateAndTime.localeCompare(b.dateAndTime));
         },
         sortMessagesDateDescending() {
+            this.artistFilter = false;
+            this.filterByBandsActive = false;
             this.notifications.sort((a, b) => b.dateAndTime.localeCompare(a.dateAndTime));
         },
-        sortMessagesByBandName() {
-            this.notifications.sort((a, b) => a.bandName.localeCompare(b.bandName));
+        sortMessagesBySelectedBand() {
+            this.filteredNotifications = this.notifications.filter((element) => element.bandName == this.bandFilterQuery);
+            this.filterByBandsActive = true;
         }
     },
-
 }
 </script>
 
